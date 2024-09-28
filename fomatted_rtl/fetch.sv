781b68f06e0a66b4c5dce4358f84e82059d7b31e
cern <vkingpro35@gmail.com>
2024-09-28 11:53:24 +0700

    fix

--- fomatted_rtl/fetch.sv
+++ fomatted_rtl/fetch.sv
@@ -12,6 +12,7 @@ module fetch #(
 
     output logic        instr_req_o,    // req
     input  logic        instr_gnt_i,    // gnt
+    input  logic [31:0] instr_addr_i
     output logic [31:0] instr_addr_o,   // addr to get instr
     input  logic [31:0] instr_rdata_i,  // rdata
     input  logic        instr_err_i,    // err    // fix NO USE
@@ -32,17 +33,13 @@ module fetch #(
 
 
   wire        instr_req;  // request for instruction
-  reg  [31:0] instr_addr;  // instruction memory address
   wire [31:0] instr_mem;  // instruction from memory
   wire        instr_ack;  // high if new instruction is now on the bus
 
-  reg  [31:0] prev_pc;
-  reg  [31:0] stalled_instr;
-  reg  [31:0] stalled_pc;
   reg         r_clk_en;
   reg         r_clk_en_d;
   reg         stall_fetch;
-  reg         stall_q;
+  reg         stall_q;  
 
   logic [DEPTH-1:0] [31:0] rdata_d,      rdata_q;
   reg   [DEPTH-1:0] [31:0] instr_addr_d, instr_addr_q; // fifo data in wire (d) and register (q)
@@ -70,7 +67,7 @@ module fetch #(
   // FIFO management //
   /////////////////////
 
-  assign pop_fifo = (~aligned_is_compressed | PC[1]);   // if there is a 16 bit instruction at 1st half, or a 32 bit instruction then fifo ready for pop
+  assign pop_fifo = (~aligned_is_compressed | pc[1]);   // if there is a 16 bit instruction at 1st half, or a 32 bit instruction then fifo ready for pop
 
   for (genvar i = 0; i < (DEPTH - 1); i++) begin : g_fifo_next
     // Calculate lowest free entry (write pointer)
@@ -89,7 +86,7 @@ module fetch #(
     /* Basically, valid_popped it just an intermediate logic to track data transmit between depth levels. We can just removed it and valid_pushed still does the trick*/
 
     // All entries are wiped out on a clear
-    assign occupied_d[i] = valid_popped[i] & ~clear_i;                          // check if data occupied in this depth or not
+    assign occupied_d[i] = valid_popped[i] & ~flush;                          // check if data occupied in this depth or not
 
     // data flops are enabled if there is new data to shift into it, or
     assign entry_en[i] = (valid_pushed[i+1] & pop_fifo) |
@@ -97,7 +94,7 @@ module fetch #(
                          (in_valid_i & lowest_free_entry[i] & ~pop_fifo);
 
     // take the next entry or the incoming data
-    assign rdata_d[i]  = occupied_q[i+1] ? rdata_q[i+1] : in_rdata_i; // if higher depth is occupied, shift down. Otherwise get new data
+    assign rdata_d[i]  = occupied_q[i+1] ? rdata_q[i+1] : instr_rdata_i; // if higher depth is occupied, shift down. Otherwise get new data
   end
 
   ///////////////////////////
@@ -117,7 +114,7 @@ module fetch #(
 
   // Instruction aligner (if unaligned)
   always_comb begin
-    if (PC[1]) begin
+    if (pc[1]) begin
       // unaligned case
       instr_send    = rdata_unaligned;
 
@@ -138,7 +135,7 @@ module fetch #(
   //////////////////////////////////////////////
 
   // Update the address on branches and every time an instruction is driven
-  assign instr_addr_en = clear_i | instr_req_o;
+  assign instr_addr_en = flush | instr_req_o;
 
   // Increment the address by two every time a compressed instruction is popped
   assign addr_incr_two = instr_addr_q[1] ? unaligned_is_compressed :
@@ -148,21 +145,21 @@ module fetch #(
                             // Increment address by 4 or 2
                             {29'd0,~addr_incr_two,addr_incr_two});
 
-  assign instr_addr_d = clear_i ? in_addr_i[31:1] :
+  assign instr_addr_d = flush ? instr_addr_i[31:1] :
                                   instr_addr_next;
 
-  assign PC      = {instr_addr_q, 1'b0};
+  assign pc      = {instr_addr_q, 1'b0};
 
   if (ResetAll) begin : g_instr_addr_ra
-    always_ff @(posedge clk_i or negedge rst_ni) begin
-      if (!rst_ni) begin
+    always_ff @(posedge clk or negedge rstn) begin
+      if (!rstn) begin
         instr_addr_q <= '0;
       end else if (instr_addr_en) begin
         instr_addr_q <= instr_addr_d;
       end
     end
   end else begin : g_instr_addr_nr
-    always_ff @(posedge clk_i) begin
+    always_ff @(posedge clk) begin
       if (instr_addr_en) begin
         instr_addr_q <= instr_addr_d;
       end
@@ -172,33 +169,7 @@ module fetch #(
   ////////////////////
   // FIFO registers //
   ////////////////////
-
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
-      occupied_q <= '0;
-    end else begin
-      occupied_q <= occupied_d;
-    end
-  end
-
-  for (genvar i = 0; i < DEPTH; i++) begin : g_fifo_regs
-    if (ResetAll) begin : g_rdata_ra
-      always_ff @(posedge clk_i or negedge rst_ni) begin
-        if (!rst_ni) begin
-          rdata_q[i] <= '0;
-        end else if (entry_en[i]) begin
-          rdata_q[i] <= rdata_d[i];
-        end
-      end
-    end else begin : g_rdata_nr
-      always_ff @(posedge clk_i) begin
-        if (entry_en[i]) begin
-          rdata_q[i] <= rdata_d[i];
-        end
-      end
-    end
-  end
-
+  
 
   /* Stall conditions
   stall this stage when:
@@ -206,13 +177,14 @@ module fetch #(
   - request but no ack yet
   - no request at all (no instruction to execute for this stage)
   */
+  assign instr_req = r_clk_en;  // request for new instruction if this stage is enabled
+  
   wire        stall_bit = (stall_fetch ||  // stall fetch
   stall ||  // stall
- (instr_req && !instr_ack) ||  // request but no ack
- !instr_req  // no request
-);
-  assign instr_req = r_clk_en;  // request for new instruction if this stage is enabled
-
+  (instr_req && !instr_ack) ||  // request but no ack
+  !instr_req  // no request
+  );
+  
   // clk enable logic for fetch stage
   wire disable_next_stage = ((alu_change_pc || writeback_change_pc) && !(stall || stall_fetch));
   always @(posedge clk, negedge rstn) begin
@@ -223,66 +195,72 @@ module fetch #(
       r_clk_en <= !disable_next_stage;
   end
 
-  /* Update registers conditions
-  update registers only if this stage is enable and next stages are not stalled
-  */
   wire enable_update_registers = ((!stall_bit && r_clk_en) ||  //
   (stall_bit && !clk_en && r_clk_en) ||  //
   (writeback_change_pc)  //
   );
+
+  always_ff @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      occupied_q <= '0;
+    end else begin
+      if (enable_update_registers) occupied_q <= stall_bit ? occupied_q : occupied_d;
+    end
+  end
+
+  for (genvar i = 0; i < DEPTH; i++) begin : g_fifo_regs
+    if (ResetAll) begin : g_rdata_ra
+      always_ff @(posedge clk or negedge rstn) begin
+        if (!rstn) begin
+          rdata_q[i] <= '0;
+        end else if (entry_en[i]) begin
+          if (enable_update_registers) rdata_q[i] <= stall_bit ? rdata_q[i] : rdata_d[i];
+        end
+      end
+    end else begin : g_rdata_nr
+      always_ff @(posedge clk) begin
+        if (entry_en[i]) begin
+          if (enable_update_registers) rdata_q[i] <= stall_bit ? rdata_q[i] : rdata_d[i];
+        end
+      end
+    end
+  end
+
+// pc and pipeline clk enable control logic
+
+  always @* begin
+    r_clk_en_d   = 0;
+    stall_fetch  = stall;  // stall when retrieving instructions need wait time prepare next PC when changing pc, then do a pipeline bubble to disable the ce of next stage
+    if (writeback_change_pc) begin
+      r_clk_en_d   = 0;
+    end else if (alu_change_pc) begin
+      r_clk_en_d   = 0;
+    end else begin
+      r_clk_en_d   = r_clk_en;
+    end
+  end
+
+  /* Update registers conditions
+  update registers only if this stage is enable and next stages are not stalled
+  */
+  
   always @(posedge clk, negedge rstn) begin
     if (!rstn) begin
       clk_en        <= 0;
-      instr_addr    <= PC_RESET;
-      prev_pc       <= PC_RESET;
-      stalled_instr <= 0;
-      pc            <= 0;
     end else begin
-      // update registers only if this stage is enabled and next stages are not stalled
-      if (enable_update_registers) begin
-        instr_addr <= instr_addr;
-        pc <= stall_q ? stalled_pc : prev_pc;
-        instr_send <= stall_q ? stalled_instr : instr_mem;
-      end
-      // flush this stage (only when not stalled) so that clock-enable of next stage is disabled at next clock cycle
       if (!stall_bit && flush) clk_en <= 0;
       //clock-enable will change only when not stalled
       else if (!stall_bit) clk_en <= r_clk_en_d;
       //if this stage is stalled but next stage is not, disable clock enable of next stage at next clock cycle (pipeline bubble)
       else if (stall_bit && !stall) clk_en <= 0;
-
       // raise stall when any of 5 stages is stalled
       stall_q <= (stall || stall_fetch);
 
-      // store instruction and PC before stalling so we can come back to these values when we need to return from stall
-      if (stall_bit && !stall_q) begin
-        stalled_pc <= prev_pc;
-        stalled_instr <= instr_mem;
-      end
-
-      // first delay to align the PC to the pipeline
-      prev_pc <= instr_addr;
     end
   end
 
 
-  // pc and pipeline clk enable control logic
-
-  always @* begin
-    instr_addr = 0;
-    r_clk_en_d   = 0;
-    stall_fetch  = stall;  // stall when retrieving instructions need wait time prepare next PC when changing pc, then do a pipeline bubble to disable the ce of next stage
-    if (writeback_change_pc) begin
-      instr_addr = writeback_next_pc;
-      r_clk_en_d   = 0;
-    end else if (alu_change_pc) begin
-      instr_addr = alu_next_pc;
-      r_clk_en_d   = 0;
-    end else begin
-      instr_addr = instr_addr + 4;
-      r_clk_en_d   = r_clk_en;
-    end
-  end
+  
 
 
 endmodule
